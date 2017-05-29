9accf1795aaadb9b3f2978b897721443d9d2552b
Rishabh Kumar <rishabhk3003@gmail.com>
2017-05-29 23:22:39 +0530

    added colors, console statements removed

--- src/components/containers/game-board.jsx
+++ src/components/containers/game-board.jsx
@@ -1,12 +1,16 @@
 import React, {Component} from 'react'
 
-const colors = [
+const COLORS = [
     "#e74c3c",
     "#27ae60",
     "#8e44ad",
     "#d35400",
     "#f1c40f"
 ]
+
+const CARDBGCOLOR = "#0B4F6C"
+
+const FIXEDCOLOR = "#757575"
 
 export default class GameBoard extends Component {
 
@@ -41,7 +45,7 @@ export default class GameBoard extends Component {
         return(
             <section className="game-board column">
                 {this.state.cards.map((card, i) => {
-                    return <Card value={card.value} active={card.active} fixed={card.fixed} onClick={e=>this._evaluate(card, i)}/>
+                    return <Card value={card.value} active={card.active} fixed={card.fixed} onClick={e=>this._evaluate(card, i)} color={card.bgcolor}/>
                 })}
             </section>
         )
@@ -64,24 +68,27 @@ export default class GameBoard extends Component {
         if(this._flippedCards.length<2) {
             this._flippedCards.push(card)
             tempArray[index].active = true
-            // console.log('FLIPPED ARRAY', this._flippedCards.length)
+            tempArray[index].bgcolor = COLORS[Math.floor(Math.random()*5)]
             this.setState({cards: tempArray})
         }
         if(this._flippedCards.length==2) {
-            // console.log('FLIPPED ARRAY MAX', this._flippedCards.length)
             if(this._flippedCards[0].value==this._flippedCards[1].value) {
-                this._flippedCards[0].fixed = true
-                this._flippedCards[1].fixed = true
-                // setTimeout(()=>{
-                    while(this._flippedCards.length) {
-                        this._flippedCards.pop()
-                    }
-                // }, 1350)
+                for(var i = 0; i < this._flippedCards.length; i++) {
+                    this._flippedCards[i].fixed = true
+                    this._flippedCards[i].bgcolor = FIXEDCOLOR
+                }
+
+                while(this._flippedCards.length) {
+                    this._flippedCards.pop()
+                }
+
             }
             else {
                 setTimeout(()=>{
-                    this._flippedCards[0].active = false
-                    this._flippedCards[1].active = false
+                    for(var i = 0; i < this._flippedCards.length; i++) {
+                        this._flippedCards[i].active = false
+                        this._flippedCards[i].bgcolor = CARDBGCOLOR
+                    }
                     this._prevIndex = null
                     let initialState = this.initialState
                     this.setState({cards: initialState.cards})
@@ -102,7 +109,12 @@ export default class GameBoard extends Component {
         let length = this.state.number
         for(var i = 0; i < 2; i++) {
             for(var j=0;j<length;j++) {
-                cards.arr.push({value: j+1, active: false, fixed: false})
+                cards.arr.push({
+                    value: j+1,
+                    active: false,
+                    fixed: false,
+                    bgcolor: "#0B4F6C"
+                })
             }
         }
 
@@ -123,12 +135,12 @@ export default class GameBoard extends Component {
 }
 
 class Card extends Component {
-    
+
     render() {
         let activeClassName = (this.props.active)?("active"):("")
         let fixedClassName = (this.props.fixed)?("fixed"):("")
         return(
-            <div className={"card "+activeClassName+" "+fixedClassName} onClick={this.props.onClick}>
+            <div className={"card "+activeClassName+" "+fixedClassName} onClick={this.props.onClick} style={{backgroundColor: this.props.color}}>
                 <span>{this.props.value || null}</span>
             </div>
         )
